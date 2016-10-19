39cd943e2177f7d368dfd9abd91e37c8b8a358ff
Elias Posen <ep2851@bard.edu>
2016-10-19 17:10:48 -0400

    Including Espresso Test

--- app/src/main/java/com/example/eliasposen/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/eliasposen/criminalintent/CrimeLab.java
@@ -25,7 +25,7 @@ public class CrimeLab {
     }
     private CrimeLab(Context context){
         mCrimes = new ArrayList<>();
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 1; i++){
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); //every other one
